b6d0b05f9b099a6b90d024693d0eea58a0a867bb
Justin Mattson <justin.mattson@outlook.com>
2020-07-29 11:48:45 -0600

    sync

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home">
-    <H1>Hi Hey Hello</H1>
-    Welcome to my blank slate...
+  <div class="home container-fluid" style="height:90vh;">
+    <div class="row">
+      <div class="col-12 p-0">
+        <H1>Hi Hey Hello</H1>Welcome to my blank slate...
+      </div>
+      <div class="col-12 bg-secondary">Column 2</div>
+    </div>
   </div>
 </template>
 
