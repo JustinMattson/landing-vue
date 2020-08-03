f17e814034031757687e1052c66ac504f3654c3f
Justin Mattson <justin.mattson@outlook.com>
2020-08-03 14:37:13 -0600

    changed router hist to hash

--- src/router/index.js
+++ src/router/index.js
@@ -38,9 +38,11 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+  // mode: "history",
+  // base: process.env.BASE_URL,
+  // routes,
+  routes: routes,
+  mode: "hash",
 });
 
 export default router;
