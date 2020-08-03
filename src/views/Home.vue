67ea233b45b452caec4a409ed5897f27b0db483c
Justin Mattson <justin.mattson@outlook.com>
2020-08-02 21:21:01 -0600

    added some more projects.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -110,25 +110,346 @@
     <!-- PROJECTS -->
     <!-- <div class="container-fluid"> -->
     <div class="row">
-      <div class="col-12" style="background-color:#777C;">
-        <H1 class="text-center py-3">Coding Projects:</H1>
+      <div class="col-12" style="background-color:#8888;">
+        <H1 class="text-center py-3 text-shadow-light">Coding Projects:</H1>
       </div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">Keepr</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-secondary" style="height:150px;">Please Help Me!</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-info" style="height:150px;">KanBan</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">BugLog</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-secondary" style="height:150px;">5</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-info" style="height:150px;">6</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">7</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-secondary" style="height:150px;">8</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-info" style="height:150px;">9</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">10</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-secondary" style="height:150px;">11</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-info" style="height:150px;">12</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">13</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-secondary" style="height:150px;">14</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-info" style="height:150px;">15</div>
-      <div class="col-12 col-md-6 col-lg-3 bg-primary" style="height:150px;">16</div>
+      <!-- Keepr -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/keepr.png"
+            class="card-img"
+            alt="keepr.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Keepr</h3>
+            <p class="card-text text-shadow">
+              <small>Vue.js | C# | MySQL</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/CS-final-keepr"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a class="ao" href="https://keepr252.herokuapp.com/#/" target="_blank">
+                <h3>HEROKU</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Volunteer #8 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/volunteer.png"
+            class="card-img"
+            alt="volunteer.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Volunteer</h3>
+            <p class="card-text text-shadow">
+              <small>Vue.js | Node.js | MongoDB</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/capstone-volunteer"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a class="ao" href="https://capstone-volunteer.herokuapp.com" target="_blank">
+                <h3>HEROKU</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- BugLog #8 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/buglog.png"
+            class="card-img"
+            alt="buglog.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">BugLog</h3>
+            <p class="card-text text-shadow">
+              <small>Vue.js | Node.js | MongoDB</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/checkpoint-buglog"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a class="ao" href="https://checkpoint-buglog.herokuapp.com" target="_blank">
+                <h3>HEROKU</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Kanban #7 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/kanban.png"
+            class="card-img"
+            alt="kanban.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Kanban</h3>
+            <p class="card-text text-shadow">
+              <small>Vue.js | Node.js | MongoDB</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/checkpoint-kanban"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a class="ao" href="https://checkpoint-kanban.herokuapp.com" target="_blank">
+                <h3>HEROKU</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Bloggr #6 -->
+      <div
+        class="col-12 col-md-6 col-lg-3 valign p-1 bg-happy"
+        style="height:150px;background-color:#8888;"
+      >
+        <div class="card bg-dark text-white">
+          <!-- <img
+            src="../assets/img/bloggr.png"
+            class="card-img"
+            alt="bloggr.png"
+            style="opacity:40%;height:145px;"
+          />-->
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Bloggr</h3>
+            <p class="card-text text-shadow">
+              <small>VUE | JS | Node.js | MongoDB</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/bloggr-checkpoint"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a
+                class="ao"
+                href="https://justinmattson.github.io/bloggr-checkpoint"
+                target="_blank"
+              >
+                <h3>GITHUB</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Hackathon #5 -->
+      <div
+        class="col-12 col-md-6 col-lg-3 valign p-1 bg-happy"
+        style="height:150px;background-color:#8888;"
+      >
+        <div class="card bg-dark text-white">
+          <!-- <img
+            src="../assets/img/hackathon.png"
+            class="card-img"
+            alt="hackathon.png"
+            style="opacity:40%;height:145px;"
+          />-->
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Scene It!</h3>
+            <p class="card-text text-shadow">
+              <small>JS | Node.js | MongoDB</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/hackathon-1"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <!-- <a class="ao" href="https://justinmattson.github.io/hackathon-1" target="_blank">
+                <h3>GITHUB</h3>
+              </a>-->
+              <h4>Group Project</h4>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Inspire #4 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/inspire.png"
+            class="card-img"
+            alt="inspire.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Inspire</h3>
+            <p class="card-text text-shadow">
+              <small>JS 81.9% | HTML 14.5% | CSS 3.6%</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/CheckPoint3-inspire"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a
+                class="ao"
+                href="https://justinmattson.github.io/CheckPoint3-inspire"
+                target="_blank"
+              >
+                <h3>GITHUB</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Task Master #3 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/taskmaster.png"
+            class="card-img"
+            alt="taskmaster.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Task Master</h3>
+            <p class="card-text text-shadow">
+              <small>JS | HTML | CSS</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/CheckPoint3-TaskMaster"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a
+                class="ao"
+                href="https://justinmattson.github.io/CheckPoint3-TaskMaster"
+                target="_blank"
+              >
+                <h3>GITHUB</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- Idle Clicker #2 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/lollipop.png"
+            class="card-img"
+            alt="lollipop.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">The Lollipop Shop</h3>
+            <p class="card-text text-shadow">
+              <small>JS | HTML | CSS</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/CheckPoint2"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a class="ao" href="https://justinmattson.github.io/CheckPoint2" target="_blank">
+                <h3>GITHUB</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- SiteClone #1 -->
+      <div class="col-12 col-md-6 col-lg-3 valign p-1" style="height:150px;background-color:#8888;">
+        <div class="card bg-dark text-white">
+          <img
+            src="../assets/img/siteclone.png"
+            class="card-img"
+            alt="siteclone.png"
+            style="opacity:40%;height:145px;"
+          />
+          <div class="card-img-overlay">
+            <h3 class="card-title text-shadow">Site Clone</h3>
+            <p class="card-text text-shadow">
+              <small>HTML | CSS | JS</small>
+            </p>
+            <span class="d-flex justify-content-between">
+              <a
+                class="ao"
+                href="https://github.com/JustinMattson/week-1-checkpoint"
+                style="text-decoration:none;"
+                target="_blank"
+              >
+                <i class="fab fa-github-square fa-2x"></i>
+              </a>
+              <a
+                class="ao"
+                href="https://justinmattson.github.io/week-1-checkpoint/"
+                target="_blank"
+              >
+                <h3>GITHUB</h3>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-3 valign bg-primary" style="height:150px;">BugLog</div>
+      <div
+        class="col-12 col-md-6 col-lg-3 valign text-light text-shadow-dark"
+        style="height:150px;"
+      ></div>
+
+      <div class="col-12 col-md-6 col-lg-3 valign bg-info bg-happy" style="height:150px;">6</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-primary" style="height:150px;">7</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-secondary" style="height:150px;">8</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-happy" style="height:150px;">9</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-primary" style="height:150px;">10</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-secondary" style="height:150px;">11</div>
+      <div class="col-12 col-md-6 col-lg-3 valign bg-info" style="height:150px;">12</div>
     </div>
     <!-- </div> -->
 
@@ -181,4 +502,28 @@ export default {
   background-size: cover;
   background-attachment: fixed;
 }
+.bg-happy {
+  background: url("../assets/img/IMAG0996_wild_sunflowers.jpg") no-repeat center
+    center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-attachment: fixed;
+}
+
+a.ao {
+  text-decoration: none;
+  color: white;
+  text-shadow: 1px 1px 5px black;
+}
+.text-shadow {
+  text-shadow: 1px 1px black;
+}
+/* .valign {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+} */
 </style>
